8cf66545fcab24e3468e3466f2436f6a77a646da
A-Fischer-Slalom <87672853+A-Fischer-Slalom@users.noreply.github.com>
2021-09-17 17:14:24 -0500

    fixing compile issue

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @ServletComponentScan
 public class HerokuApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(HerokuApplication.class, args);
     }
 }
