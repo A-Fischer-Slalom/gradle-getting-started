a846182a53e9b4bea43830e6b200580ac7145bd2
A-Fischer-Slalom <87672853+A-Fischer-Slalom@users.noreply.github.com>
2021-09-17 17:28:56 -0500

    fixed compilation

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,6 +22,9 @@ import java.util.Map;
 @SpringBootApplication
 @ServletComponentScan
 public class HerokuApplication {
+    public boolean someLibraryMethod() {
+        return true;
+    }
     public static void main(String[] args) {
         SpringApplication.run(HerokuApplication.class, args);
     }
--- src/test/java/com/example/heroku/HerokuApplicationTests.java
+++ src/test/java/com/example/heroku/HerokuApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.heroku;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,9 +10,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class HerokuApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Test public void someLibraryMethodReturnsTrue() {
+        HerokuApplication classUnderTest = new HerokuApplication();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
 
 }
 
